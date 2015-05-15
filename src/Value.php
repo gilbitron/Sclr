2e4b646235bd45107e4729836a11db4508cce5bd
Gilbert Pellegrom <gilbert@pellegrom.me>
2015-05-15 12:56:11 +0100

    Add type

--- src/Value.php
+++ src/Value.php
@@ -3,16 +3,38 @@
 class Value {
 
 	protected $value;
+	protected $type;
 
 	public function __construct($value)
 	{
 		$this->value = $value;
+		$this->type = 'string';
+
+		if (is_array($value)) {
+			$this->type = 'array';
+		} elseif (is_object($value)) {
+			$this->type = 'object';
+		} elseif (is_float($value)) {
+			$this->type = 'float';
+		} elseif (is_int($value)) {
+			$this->type = 'integer';
+		} elseif (is_bool($value)) {
+			$this->type = 'boolean';
+		} else {
+			$this->value = (string)$value;
+		}
+
 		$this->init();
 	}
 
 	protected function init()
 	{
 
+	}
+
+	public function getType()
+	{
+		return $this->type;
 	}
 
 }
